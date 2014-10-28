2f998d40d933da18539c7cc3886db3ef0bc1eefb
Andrew R Couture <AMARNA@Andrews-Mac-mini.local>
2014-10-28 09:22:28 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Andrew R Couture on 2014-10-28.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
