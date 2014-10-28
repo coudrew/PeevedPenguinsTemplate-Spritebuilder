1228c3506d239711053d35b1b8ece1c567a594c1
Andrew R Couture <AMARNA@Andrews-Mac-mini.local>
2014-10-28 10:28:27 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -8,27 +8,6 @@
 
 #import "Gameplay.h"
 
-@implementation Gameplay {
-CCPhysicsNode *_physicsNode;
-CCNode *_catapultArm;
-}
-
-- (void)didLoadFromCCB {
-    self.userInteractionEnabled = TRUE;
-}
-
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    [self launchPenguin];
-}
-
-- (void)launchPenguin {
-    CCNode* penguin = [CCBReader load:@"Penguin"];
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
-    [_physicsNode addChild:penguin];
-    //create and apply force
-    CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 8000);
-    [penguin.physicsBody applyForce:force];
-}
+@implementation Gameplay 
 
 @end
