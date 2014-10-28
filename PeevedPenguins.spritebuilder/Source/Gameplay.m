99c17eef4dd07a63fb1a854d57abc878a2f6218a
Andrew R Couture <AMARNA@Andrews-Mac-mini.local>
2014-10-28 10:39:23 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -17,4 +17,14 @@
     self.userInteractionEnabled = TRUE;
 }
 
+- (void)launchPenguin {
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
 @end
