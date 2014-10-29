9e6761fcdf48fe2160cad391df7617fcf9298432
Andrew R Couture <AMARNA@Andrews-Mac-mini.local>
2014-10-29 10:04:02 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Andrew R Couture on 2014-10-29.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
